4f53902c7deadd1fe3dd2b06eca9227210137082
Christopher Kolstad <christopher.kolstad@finn.no>
2012-06-26 13:38:36 +0200

    Fixed spec helper

--- spec/models/movie_spec.rb
+++ spec/models/movie_spec.rb
@@ -1,5 +1,14 @@
 require 'spec_helper'
 
 describe Movie do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it "allows to add a movie" do
+    m = Movie.create{:title => "Matrix", :release_year => 1999}
+    m.title.should eq("Matrix")
+  end
+  
+  it "should retrieve a movie by title" do
+    m = Movie.create{:title => "Matrix", :release_year => 1999}
+    f = Movie.find(:first, :criteria => {:title => "Matrix"})
+    f.should eq(m)
+  end
 end
