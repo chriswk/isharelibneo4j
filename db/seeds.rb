40efe0283187e868f4a82417e8bfd354defe1e15
Christopher Kolstad <christopher.kolstad@gmail.com>
2012-09-01 20:42:32 +0200

    Seed movies instead of actors

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,4 +1,4 @@
-[55751, 524, 56825, 976, 1813, 1892].each do |person_id|
-  p = Person.find_or_create_by(:tmdb_id => person_id)
-  p.update_from_tmdb
+[603, 24428, 15268].each do |movie_id|
+  m = Movie.find_or_create_by(:tmdb_id => movie_id)
+  m.update_from_tmdb
 end
