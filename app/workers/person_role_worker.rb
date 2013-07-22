ea422ec083cf086e84a1a215144ed71b53739bcf
Christopher Kolstad <christopher.kolstad@gmail.com>
2013-07-22 21:13:34 +0200

    Removed outdated workers

--- app/workers/person_role_worker.rb
+++
@@ -1,9 +0,0 @@
-class PersonRoleWorker
-  include Sidekiq::Worker
-
-  def perform(movie, role)
-    person = Person.find_or_create_by(:tmdb_id => role.id)
-    person.map_movie_role(movie, role)
-    person.save
-  end
-end
